27700dc84c2bb2e4da4a6eaed49a45cd7971ac85
NazifaPushon <mashiat.pushon@gmail.com>
2022-03-20 23:31:51 +0600

    factorial

---
+++ lecture-6/fibonacci.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+void fib(int num){
+    int t1 =0;
+    int t2 = 1;
+
+    int nextTern;
+    for(int i = 1; i<= num; i++){
+        cout << t1 << endl;
+        nextTern = t1 + t2;
+        t1= t2;
+        t2 = nextTern;
+    }
+    return;
+}
+
+int main () {
+    int n;
+    cin >> n;
+
+    fib(n);
+    return 0;
+}
