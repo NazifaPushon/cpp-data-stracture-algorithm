876533365dfc044768c4c10627a66a593cddab15
NazifaPushon <mashiat.pushon@gmail.com>
2022-03-27 23:42:16 +0600

    2 d array project

--- lecture-9/2D_Array.cpp
+++ lecture-9/2D_Array.cpp
@@ -1,25 +1,33 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
-int main () {
-    int n, m;
-    cin >>n >> m;
 
-    int arr[n][m];
-
-    for (int i = 0; i< n;i++){
-        for(int j = 0; j < m;j++){
-            cout << "Giving input for " << i << " " << j <<" : " ;
-            cin >> arr[i][j];
+int main() {
+    int x,y,s= 0;
+    cin >> x >> y;
+    int *arr[x];
+    int itaretor1 = x;
+    int itaretor2 = y;
+    while(itaretor1 >= 0){
+        int num;
+        cin >> num;
+        arr[s] = new int[num];
+        for(int i = 0; i < num;i++){
+            cin >> arr[s][i];
         }
+        s++;
+        itaretor1--;
     }
-
-
-    for (int i = 0; i< n;i++){
-        for(int j = 0; j < m;j++){
-            cout << arr[i][j] << " ";
-        }
-        cout << endl;
+    while(itaretor2 >= 0){
+        int a ,b;
+        cin >> a >> b;
+        cout << arr[a][b] << endl;
+        itaretor2--;
     }
-
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    return 0;
 }
